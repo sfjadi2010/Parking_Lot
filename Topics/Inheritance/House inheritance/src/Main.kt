20288b4172fc82e4d2952f935a9f1ac7d8bb4167
Salman Farsi <salman.farsi@certara.com>
2021-11-17 20:05:11 -0500

    Completed House price problem.

--- Topics/Inheritance/House inheritance/src/Main.kt
+++ Topics/Inheritance/House inheritance/src/Main.kt
@@ -5,4 +5,17 @@ fun main() {
     println(totalPrice(house))
 }
 
-class House(val rooms: Int, val price: Int) { }
+class House(val rooms: Int, val price: Int) {}
+
+fun totalPrice(house: House): Int {
+    val rooms = house.rooms
+    val price = if (house.price < 0) 0 else if (house.price > 1_000_000) 1_000_000 else house.price
+    return when {
+        rooms <= 1 -> (1 * price).toInt()
+        rooms in 2..3 -> (1.2 * price).toInt()
+        rooms == 4 -> (1.25 * price).toInt()
+        rooms in 5..7 -> (1.4 * price).toInt()
+        rooms in 8..10 -> (1.6 * price).toInt()
+        else -> (1.6 * price).toInt()
+    }
+}
